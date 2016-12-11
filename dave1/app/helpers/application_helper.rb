3bc5b2bb5bda6a77ef231ed222a48ab8dd53b7bf
Dacks <math.milliken@gmail.com>
2016-12-11 23:56:18 +0000

    end of lecture 6

--- dave1/app/helpers/application_helper.rb
+++ dave1/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def nav_link_to ( click_text, action, url, style )
+        if action_name == action
+           return link_to click_text, url, :class => style
+        else
+            return link_to click_text, url
+        end
+    end
+    
+    
 end
